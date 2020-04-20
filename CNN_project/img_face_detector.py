3a871efe8fcf7812b7bf155f681540793b950398
ChanOh Jeong <pooo1002@naver.com>
2020-04-20 17:28:37 +0900

    Dog_face0420

--- CNN_project/img_face_detector.py
+++ CNN_project/img_face_detector.py
@@ -3,7 +3,11 @@ import os
 import pathlib
 import glob
 import cv2
+import dlib
+import numpy as np
+import matplotlib.pyplot as plt
 import settings
+from imutils import face_utils
 
 def load_name_images(image_path_pattern):
     name_images = []
@@ -69,6 +73,52 @@ def delete_dir(dir_path, is_delete_top_dir=True):
     if is_delete_top_dir:
         os.rmdir(dir_path)
 
+
+
+
+
+def detect_dog_face(file_path, image): 
+    # 이미지 파일의 Grayscale화
+    image_gs = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    detector = dlib.cnn_face_detection_model_v1('dogHeadDetector.dat')
+    predictor = dlib.shape_predictor('landmarkDetector.dat')
+
+    # 강아지 얼굴 인식
+    faces = detector(image_gs, upsample_num_times=1)
+    if len(faces) == 0:
+        print("Fail to detecting face")
+        return
+    print("Success Face Detection!")
+    # TO-DO 
+    # [76 31 83 83] -> x_pos, y_pos, width, height
+    face_count = 1
+    for(i, d) in enumerate(faces):
+        x1, y1 = d.rect.left(), d.rect.top()
+        x2, y2 = d.rect.right(), d.rect.bottom()
+        face_image = image[y1:y2,x1:x2] # y, x
+        if face_image.shape[0] > 64:
+            face_image = cv2.resize(face_image,(64,64))
+        print(face_image.shape)
+            # Save 00_001.jpg -> 00_001_(face_count).jpg
+
+        path = pathlib.Path(file_path)
+        directory = str(path.parent.resolve())
+        filename = path.stem
+        extension = path.suffix
+        output_path = os.path.join(directory,f"{filename}_{face_count:03}-{extension}")
+        print(f"=================================OUTPUT File(절대 경로) : {output_path}")
+        try:
+            cv2.imwrite(output_path,face_image)
+            face_count = face_count + 1
+        except:
+            print("Exception occured : {}".format(output_path))
+    return
+
+
+
+
+
+
 RETURN_SUCCESS = 0
 RETURN_FAILURE = -1
 # Origin Image Pattern
@@ -97,8 +147,8 @@ def main():
         file_path = os.path.join(OUTPUT_IMAGE_DIR,f"{name_image[0]}")
         image = name_image[1]#실제 image 파일 
 
-        cascade_filepath = settings.CASCADE_FILE_PATH
-        detect_image_face(file_path, image, cascade_filepath)
+        # cascade_filepath = settings.CASCADE_FILE_PATH
+        detect_dog_face(file_path, image)
     #     # TO-DO 
 
 
